30dc83406c896a65f493363edfcc56c809347b4c
Guilherme Oliveira <oliveiraguilherme@ufg.br>
2023-11-24 15:06:50 -0300

    Iterção 1 e 2

--- index.js
+++ index.js
@@ -1,7 +1,18 @@
 // Iteration 1: Names and Input
-
+const hacker1 = "Guilherme";
+console.log(`The drivers name is ${hacker1}`);
+const hacker2 = "Ronaldo";
+console.log(`The navigator is ${hacker2}`);
 
 // Iteration 2: Conditionals
+
+if(hacker1.length < hacker2.length){
+  console.log(`Drivers name tem ${hacker2.length} letras`)
+} else if(hacker1.length == hacker2.length ) {
+  console.log(`Navigator tem o mesmo tamanho do Driver`)
+} else {
+  console.log(`Navigator tem ${hacker1.length} letras.`);
+}
 
 
 // Iteration 3: Loops
